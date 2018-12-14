66ed95c242ac1d37ec4a904f369b9658d90c6f2a
Aaron Jensen <ajensen@webmd.net>
2018-12-14 09:16:39 -0800

    trying to figure out why Whiskey can't find UploadFile when running under .NET Core.

--- Whiskey/Functions/Publish-WhiskeyPesterTestResult.ps1
+++ Whiskey/Functions/Publish-WhiskeyPesterTestResult.ps1
@@ -18,14 +18,12 @@ function Publish-WhiskeyPesterTestResult
     }
 
     $webClient = New-Object 'Net.WebClient'
-    $webClientType = $webClient.GetType()
-    $uploadFileMethod = $webClientType.GetMethod('UploadFile',[type[]]@([uri],[string]))
     [uri]$uploadUri = 'https://ci.appveyor.com/api/testresults/nunit/{0}' -f $env:APPVEYOR_JOB_ID
     Resolve-Path -Path $Path -ErrorAction Stop |
-        Select-Object -ExcludeProperty 'ProviderPath' |
+        Select-Object -ExpandProperty 'ProviderPath' |
         ForEach-Object { 
-            [string]$resultPath = $_
-            Write-Verbose -Message ('Uploading Pester test result file "{0}" to AppVeyor at "{1}"g.' -f $resultPath,$uploadUri)
+            $resultPath = $_
+            Write-Verbose -Message ('Uploading Pester test result file "{0}" to AppVeyor at "{1}".' -f $resultPath,$uploadUri)
             # PowerShell Core can't find the UploadFile method, so we have to use refelction.
             $webClient.UploadFile($uploadUri,$resultPath)
         }
